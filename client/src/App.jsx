281b0ba5c842db22bf4af27e1e467f4b3ce313ab
Ibad Khan <ibadkhansherani@gmail.com>
2025-08-18 08:23:36 -0700

    first commit,added navbar and header in frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Home from './pages/Home.jsx';
+import Blog from './pages/Blog.jsx';
+import {Routes,Route} from 'react-router-dom'
+
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/blog' element={<Blog />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
