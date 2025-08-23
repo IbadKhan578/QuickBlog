d79eb89024a1c0ecb83b8c1bea4c58a3207b7b70
Ibad Khan <ibadkhansherani@gmail.com>
2025-08-23 00:49:34 -0700

    defied the folder structure for admin panel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Home from './pages/Home.jsx';
 import Blog from './pages/Blog.jsx';
+import Layout from './pages/admin/Layout.jsx'
+import Dashboard from './pages/admin/Dashboard.jsx'
+import Addblog from './pages/admin/Addblog.jsx'
+import ListBlog from './pages/admin/ListBlog.jsx'
+import Comments from './pages/admin/Comments.jsx'
 import {Routes,Route} from 'react-router-dom'
 
 
@@ -10,6 +15,12 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/blog/:id' element={<Blog />} />
+        <Route path='/admin' element={<Layout /> }>
+          <Route index  element={<Dashboard />} />
+          <Route path='addBlog'  element={<Addblog />} />
+          <Route path='listBlog'  element={<ListBlog />} />
+          <Route path='comments'  element={<Comments />} />
+        </Route>
       </Routes>
     </div>
   )
