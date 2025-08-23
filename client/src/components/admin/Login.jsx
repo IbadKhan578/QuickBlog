da07e6da1bd6b463421fd53879f96c08e9de3807
Ibad Khan <ibadkhansherani@gmail.com>
2025-08-23 08:09:22 -0700

    added login page

--- client/src/components/admin/Login.jsx
+++ client/src/components/admin/Login.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaRegEye } from "react-icons/fa";
+import { ImEyeBlocked } from "react-icons/im";
+
+
 
 function Login() {
+
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const [visibility,  setvisibility] = useState(false);
+
+  const handleSubmit =  async(e)=>{
+    e.preventDefault();
+  }
   return (
-    <div>Login</div>
+    <div className='flex justify-center items-center h-screen'>
+      <div className='w-full max-w-sm p-6  max-md:m-6 border border-primary/30 shadow-xl shadow-primary/15 rounded-lg'>
+      <div className='flex flex-col items-center justify-center  '>
+        <div className='w-full py-6 text-center' >
+          <h1 className='text-3xl font-bold' > <span className='text-primary'>Admin </span> Login</h1>
+          <p>Enter your credentials to access the admin panel</p>
+        </div>
+        <form onSubmit={handleSubmit} className='mt-6 w-full sm:max-w-md text-gray-600' >
+          <div className='flex flex-col'>
+            <label>Email</label>
+          <input onChange={(e)=> setEmail(e.target.value)} value={email} className='border-b-2 border-gray-300 p-2 outline-none mb-6 ' type="email" placeholder='your email id' required/>
+          </div>
+
+           <div className='flex flex-col relative'>
+            <label>Password</label>
+          <input onChange={(e)=> setPassword(e.target.value)} value={password} className='border-b-2 border-gray-300 p-2 outline-none mb-6 ' type={visibility ? 'text' : 'password' } placeholder='your password' required/>
+            <div onClick={()=> setvisibility(prev=>!prev)} className='absolute right-7 bottom-10 cursor-pointer'>
+               { (visibility)?   <ImEyeBlocked /> :<FaRegEye /> 
+               }
+                </div>
+          </div>
+          <button  type='submit'className='w-full bg-primary text-white font-medium rounded cursor-pointer py-3 hover:bg-primary/90 ' >Login</button>
+
+        </form>
+      </div>
+      </div>
+    </div>
   )
 }
 
-export default Login
+export default Login 
