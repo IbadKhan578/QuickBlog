0e4ea8db3f5061d8b844ab558eb44adbeffe172a
Ibad Khan <ibadkhansherani@gmail.com>
2025-08-22 21:39:39 -0700

    added newsletter component and blogcards

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
+import Bloglist from '../components/bloglist'
+import Blogcard from '../components/Blogcard'
+import Newsletter from '../components/Newsletter'
 
 function Home() {
   return (
     <>
     <Navbar />
     <Header />
+    <Bloglist />
+    <Newsletter />
     </>
   )
 }
