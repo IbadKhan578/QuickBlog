8ebce2e6e4ba82147ac4330898ae7792496ed931
Ibad Khan <ibadkhansherani@gmail.com>
2025-08-24 08:52:30 -0700

    Added Db schema for blog

---
+++ server/models/blog.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const blogSchema = mongoose.Schema({
+    title: {type:String , required:true},
+    subTitle:{type:String},
+    description:{type:String , required:true },
+    category:{type:String , required:true },
+    image:{type:String , required:true },
+    ispublished:{type:Boolean, required:true}
+  
+
+},{timestamps:true});
+
+const Blog = mongoose.model('blog',blogSchema);
+export default Blog;
