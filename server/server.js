4fd811dadfeea5c2c73c52263c6a1fbac058bcc5
Ibad Khan <ibadkhansherani@gmail.com>
2025-08-24 08:51:32 -0700

    setup server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import 'dotenv/config'
 import cors from 'cors';
+import connectDB from './configs/db.js';
+import adminRouter from './routes/adminRoute.js';
 
 const app = express();
+await connectDB();
 
 // middlleware
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/admin',adminRouter)
 
 app.get('/',(req,res)=>{
 
