81955effbc9b3abe8679b9e52f44ae0bd416ad7d
Ibad Khan <ibadkhansherani@gmail.com>
2025-08-26 10:25:16 -0700

    Admin routes

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -1,11 +1,18 @@
 
 
 import express from 'express';
-import adminLogin from '../controllers/adminController.js';
+import  { adminLogin,  ApproveCommentById,  deleteCommentById,  getAllBlogAdmin,  getAllCommentAdmin, getDashboard } from '../controllers/adminController.js';
+import auth from '../middleware/auth.js';
 
 
 const adminRouter = express.Router();
 
 adminRouter.post('/login',adminLogin);
+adminRouter.get('/comments', auth ,getAllCommentAdmin);
+adminRouter.get('blogs',auth,getAllBlogAdmin);
+adminRouter.post('/delete-comment',auth,deleteCommentById);
+adminRouter.post('/approve-commnet',auth,ApproveCommentById);
+adminRouter.get('/dashboard',auth,getDashboard);
+
 
 export default adminRouter;
