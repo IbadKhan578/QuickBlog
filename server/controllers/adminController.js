c92e9cdf335bc681e0c1ed28df28f99b834abe73
Ibad Khan <ibadkhansherani@gmail.com>
2025-08-26 10:24:32 -0700

    Added all admin controllers function

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,6 +1,8 @@
 import jwt from 'jsonwebtoken';
+import Blog from '../models/blog.js';
+import comment from '../models/Comment.js';
 
-const adminLogin = async ( req,res)=>{
+export const adminLogin = async ( req,res)=>{
     const {email,password} = req.body;
     try {
         if(email!== process.env.ADMIN_EMAIL || password!== process.env.ADMIN_PASSWORD ){
@@ -18,4 +20,70 @@ const adminLogin = async ( req,res)=>{
 
 }
 
-export default adminLogin;
+
+export const getAllBlogAdmin = async (req,res)=>{
+    try {
+        const blogs = await Blog.find({}).sort({createdAt:-1})
+        res.json({success:true, blogs})
+    } catch (error) {
+                 return res.json({success:false , message:error.message});
+
+    }
+
+}
+
+export const  getAllCommentAdmin = async (req,res)=>{
+    try {
+        const comments = await comment.find({}).populate("blog").sort({createdAt:-1})
+        res.josn({success:false, comments})
+    } catch (error) {
+        res.json({success:false , message:error.message })
+    }
+
+}
+
+export const getDashboard =  async (req, res)=>{
+    try {
+        const recentBlogs = await Blog.find({}).sort({createdAt:-1}).limit(5);
+        const blogs = await Blog.countDocument();
+        const comments = await comment.countDocument();
+        const drafts = await Blog.countDocument({isPublished:false})
+        const dashboardData = {
+            recentBlogs , blogs , comments , drafts , dashboardData
+        }
+        res.josn({succeess:true, dashboardData  })
+    } catch (error) {
+      res.json({success:false , message:error.message })
+
+    }
+
+}
+
+
+export const deleteCommentById = async (req,res)=>{
+    try {
+        const {id} = req.body;
+        await comment.findByIdAndDelete(id)
+        res.json({succeess:true, message:" Comment deleted Successfully" })
+        
+    } catch (error) {
+    res.json({success:false , message:error.message })
+
+    }
+
+}
+
+
+export const ApproveCommentById = async (req,res)=>{
+    try {
+        const {id} = req.body;
+        await comment.findByIdAndUpdate(id, {isApproves:true})
+        res.json({succeess:true, message:" Comment Updated Successfully" })
+        
+    } catch (error) {
+    res.json({success:false , message:error.message })
+
+    }
+
+}
+
