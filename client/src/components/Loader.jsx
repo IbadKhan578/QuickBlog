801f80123e7a297825370070b98a90f47af9c160
Ibad Khan <ibadkhansherani@gmail.com>
2026-02-26 20:23:51 +0500

    tweak

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import Box from '@mui/material/Box';
-import Skeleton from '@mui/material/Skeleton';
+import CircularProgress from "@mui/material/CircularProgress";
+
 
 function Loader() {
   return (
-   <Box sx={{ width: 300 }}>
-      <Skeleton />
-      <Skeleton animation="wave" />
-      <Skeleton animation={false} />
-    </Box>
+          <CircularProgress color='inherit' disableShrink />
+
   )
 }
 
